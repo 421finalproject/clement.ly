b426dab5acf9fff34b05147f90a731c85748184a
Katie Chai <kchai@unc.edu>
2024-10-14 02:27:37 -0400

    Style updates

--- app/main.py
+++ app/main.py
@@ -1,4 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Task(BaseModel):
+    tid: int
+    tname: str
+    ttype: int 
+    status: int
+    start_time: int
+    end_time: int
+    
 
 app = FastAPI()
 
@@ -11,3 +22,22 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+# create task, user
+@app.post("/task/")
+async def create_task(task: Task):
+    return task
+
+
+# read data for task, user
+# @app.get("/")
+
+
+# update task, user
+# @app.put("/")
+
+
+# delete task, user
+# @app.delete("/")
+
