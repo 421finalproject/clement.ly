99964886757a2f7563c1739aa398687f3a2814e4
Vincent Zhou <zhouvincent11@gmail.com>
2024-11-09 15:43:16 -0500

    Added stubs for main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-
-
-class Task(BaseModel):
-    tid: int
-    tname: str
-    ttype: int 
-    status: int
-    start_time: int
-    end_time: int
-    
+from mysql import connector
+import os
 
 app = FastAPI()
 
@@ -23,21 +14,75 @@ async def root():
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
 
+def get_db_connection():
+    database = os.environ.get('MYSQL_DATABASE')
+    user = os.environ.get('MYSQL_USER')
+    password = os.environ.get('MYSQL_PASSWORD')
 
-# create task, user
-@app.post("/task/")
-async def create_task(task: Task):
-    return task
+    # print(database, user, password)
 
+    cnx = connector.connect(
+        user=user,
+        password=password,
+        host='mysql',
+        database=database
+    )
 
-# read data for task, user
-# @app.get("/")
+    return cnx
 
+user_table = """
+CREATE TABLE Users(
+	uid INT PRIMARY KEY,
+	uname TEXT UNIQUE,
+	password TEXT
+);
+"""
 
-# update task, user
-# @app.put("/")
+task_type_table = """
+CREATE TABLE Task_types(
+	ttid INT PRIMARY KEY,
+	uid INT,
+	Name TEXT,
+	FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE 
+)
+"""
 
+task_table = """
+CREATE TABLE Tasks(
+    tid INT PRIMARY KEY,
+ 	uid INT,
+	task_name TEXT,
+	task_type INT,
+	status INT,
+	start DATETIME,
+	end DATETIME,
+	FOREIGN KEY (uid) REFERENCES Users(uid) ON DELETE CASCADE,
+	FOREIGN KEY (task_type) REFERENCES Task_types(ttid) ON DELETE CASCADE
+)
+"""
 
-# delete task, user
-# @app.delete("/")
+@app.get("/show_tables")
+async def show_tables():
+    cnx = get_db_connection()
+
+    cursor = cnx.cursor()
+    cursor.execute("SHOW TABLES;")
+    ret = cursor.fetchall()
+
+    cnx.close()
+    return {"message": f"{ret}"}
+
+@app.get("/create_tables")
+async def create_tables():
+    cnx = get_db_connection()
+
+    cursor = cnx.cursor()
+    cursor.execute(user_table)
+    cursor.execute(task_type_table)
+    cursor.execute(task_table)
+    cnx.commit()
+    ret = cursor.fetchall()
+
+    cnx.close()
+    return {"message": f"{ret}"}
 
