02b511f51e69f931a6f4b7f4e458733c5b314078
Katie Chai <kchai@unc.edu>
2024-11-16 17:10:20 -0500

    working with vincent and varun on the javascript yay

--- app/main.py
+++ app/main.py
@@ -3,9 +3,19 @@ from mysql import connector
 import os
 import hashlib
 import datetime
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
